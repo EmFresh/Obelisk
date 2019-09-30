5cc2fa813bcd7d3d0dd23e96912fd084198b4e70
Lillian <ziqi.fan@ontariotech.net>
2019-09-30 11:47:07 -0400

    Add Some Comments

--- Assets/_Scripts/CameraMovement.cs
+++ Assets/_Scripts/CameraMovement.cs
@@ -18,13 +18,17 @@ public class CameraMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //Assign Camera distence offset
         _cameraBFOffset = transform.position - PlayerTransform.position;
 
+        //Make pivot as a child to player and move to player's position
         pivot.transform.position = PlayerTransform.transform.position;
         pivot.transform.parent = PlayerTransform.transform;
 
+        //Make you cursor dispear when play game
         Cursor.lockState = CursorLockMode.Locked;
 
+        //Assign moveup and move right offset to camera
         transform.position += transform.right * _cameraLROffset;
         transform.position += transform.up * _cameraUDOffset;
     }
@@ -32,29 +36,34 @@ public class CameraMovement : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
+        //Remove moveup and move right offset to help calculation
         transform.position -= transform.right * _cameraLROffset;
         transform.position -= transform.up * _cameraUDOffset;
 
+        //Get X position of the mouse and rotate the player
         float horizontal = Input.GetAxis("Mouse X") * RotateSpeed;
         PlayerTransform.Rotate(0, horizontal, 0);
 
+        //Get X position of the mouse and rotate the pivot
         float vertical = Input.GetAxis("Mouse Y") * RotateSpeed;
         pivot.Rotate(-vertical, 0, 0);
 
+        //Move the camera based on current ratation of player and the sidtance offset, slerp help movement smoother
         float desiredYAngle = PlayerTransform.eulerAngles.y;
         float desiredXAngle = pivot.eulerAngles.x;
-
         Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
         transform.position = Vector3.Slerp(transform.position, PlayerTransform.position + (rotation * _cameraBFOffset), SmoothFactor);
         
-
+        //Make sure camera not going below ground
         if(transform.position.y < PlayerTransform.position.y)
         {
             transform.position = new Vector3(transform.position.x, PlayerTransform.position.y, transform.position.z);
         }
 
+        //Rotate camera to player's location
         transform.LookAt(PlayerTransform);
 
+        //Assign moveup and move right offset to camera
         transform.position += transform.right * _cameraLROffset;
         transform.position += transform.up * _cameraUDOffset;
     }
--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -16,21 +16,26 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //Assign player's phisics body and collider
         rb = GetComponent<Rigidbody>();
         col_size = GetComponent<BoxCollider>();
+        
         isGrounded = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Get the X and Y position of any input (laptop or controller)
         var x = Input.GetAxis("Horizontal");
         var y = Input.GetAxis("Vertical");
 
+        //Move player to that direction, forward is allways where player look at
         moveDirection = transform.forward * y + transform.right * x;
         moveDirection = moveDirection.normalized;
         transform.position += moveDirection * MaxSpeed * Time.deltaTime;
        
+        //If player is on the ground make player jump
         if(Input.GetKey(KeyCode.Space) && isGrounded == true)
         {
             rb.AddForce(0, JumpHeight, 0);
@@ -38,6 +43,7 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    // Check player on the ground or not (Unity build in function)
     private void OnCollisionEnter(Collision collision)
     {
         isGrounded = true;
