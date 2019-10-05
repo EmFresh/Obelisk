16c4589db2f51c4063261b55b34a14946b3d825a
EmFresh <emory.wynn@uoit.net>
2019-10-04 22:30:02 -0400

    [Started] The beginning of tower building

---
+++ Assets/_Scripts/TowerBuild.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class TowerBuild : MonoBehaviour
+{
+
+    public List<GameObject> towerParts = new List<GameObject>();
+    public KeyCode key;
+    private Transform theParent;
+
+    public int stage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        theParent = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerStay(Collider obj)
+    {
+        Debug.Log("entered build site");
+         if (obj.gameObject.tag.Contains("BuildZone"))
+            if (Input.GetKeyDown(key))
+            {
+                 Debug.Log("build the tower");
+                GameObject towerPart = Instantiate(towerParts[0]);
+                towerPart.transform.parent = theParent;
+                theParent = towerPart.transform;
+                stage++;
+            }
+    }
+
+}
