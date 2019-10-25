dc9a6604ecdfec981a1a065d6fa8ee47d9592fa5
Rob <robert.andersen@ontariotechu.net>
2019-10-25 01:43:14 -0400

    The rogue can now touch the chests and when it does the resources transfer from the rogue to the respective chest. BASIC UI shows the resources in each chest

---
+++ Assets/_Scripts/ResourcesInChestCrystal.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourcesInChestCrystal : MonoBehaviour
+{
+    // Start is called before the first frame update
+int woodStock = 0;
+    int stoneStock = 0;
+    int crystalStock = 0;
+
+    private Text crystalText;
+    void Start()
+    {
+         crystalText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        crystalText.text = "Crystal: " + PlayerPickup.crystalStock;
+    }
+
+}
---
+++ Assets/_Scripts/ResourcesInChestWood.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class ResourcesInChestWood : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    int woodStock = 0;
+    int stoneStock = 0;
+    int crystalStock = 0;
+
+    private Text woodText;
+    void Start()
+    {
+         woodText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        woodText.text = "Wood: " + PlayerPickup.woodStock;
+    }
+}
