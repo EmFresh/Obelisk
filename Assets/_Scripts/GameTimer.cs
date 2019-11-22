a003f8858728b8d2d12d9444c3879488827f60f2
Rob <robert.andersen@ontariotechu.net>
2019-11-21 20:35:29 -0500

    Basic Game Timer Implemented in UI

---
+++ Assets/_Scripts/GameTimer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    float counter = 300.0f;
+    public Text countText;
+
+    bool timeOut = false;
+    void Start()
+    {
+        countText = gameObject.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (counter > 0)
+        {
+            counter -= Time.deltaTime;
+            countText.text = "Time Left: " + Mathf.Round(counter);
+        }
+        if (counter <= 0)
+        {
+            timeOut = true;
+        }
+    }
+}
