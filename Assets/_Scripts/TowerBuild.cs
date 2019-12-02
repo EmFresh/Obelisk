a32c761681c82155b60d1d88cb483c3d3ba8f4ed
EmFresh <emory.wynn@uoit.net>
2019-12-02 16:42:45 -0500

    [Fixed] the tower building works

--- Assets/_Scripts/TowerBuild.cs
+++ Assets/_Scripts/TowerBuild.cs
@@ -23,6 +23,7 @@ public class TowerBuild : MonoBehaviour
     private Transform theParent;
     private const int maxStages = 3;
 
+    private bool initBuild = true;
     void OnTriggerStay(Collider obj)
     {
         playerIndex = obj.gameObject.GetComponent<PlayerMovement>().playerIndex;
@@ -41,23 +42,28 @@ public class TowerBuild : MonoBehaviour
 
                         //get the next Parent
                         theParent = gameObject.transform;
-                        if(theParent.childCount != 3)
-                        while (theParent.childCount > 0) theParent = theParent.GetChild(theParent.childCount-1);
+                        if (theParent.childCount != 3)
+                            while (theParent.childCount > 0) theParent = theParent.GetChild(theParent.childCount - 1);
 
                         GameObject towerPart = Instantiate(towerParts[stage % towerParts.Count]);
 
                         //sets the tower part as the child of the previous object
                         towerPart.transform.parent = theParent;
-                        towerPart.transform.position = theParent.position;
 
+                        towerPart.transform.position = theParent.position;
+                        //if (theParent.childCount > 0)
+                        //    theParent = theParent.GetChild(theParent.childCount - 1);
+                        
+                        
                         //get the size of the two objects
                         Bounds obj1 = theParent.gameObject.GetComponent<MeshRenderer>().bounds;
                         Bounds obj2 = towerPart.GetComponent<MeshRenderer>().bounds;
                         Vector3 size1 = obj1.max - obj2.min, size2 = obj2.max - obj2.min;
 
                         //place the new object on top of the old one
-                        towerPart.transform.position += new Vector3(0, (size1.y + size2.y) * 0.5f, 0);
+                        towerPart.transform.position += new Vector3(0, (size1.y * .5f + size2.y * .5f) + 3 * (initBuild ? 1 : 0), 0);
 
+                        initBuild = false;
                         //increase the build stage
                         WizardResourceManager.wizardWoodAmount -= woodNeeded;
                         WizardResourceManager.wizardStoneAmount -= stoneNeeded;
