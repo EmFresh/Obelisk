d95a2d6bd3fdeec67723bbd9bae077b3def88ca3
Rob <robert.andersen@ontariotechu.net>
2020-01-22 11:23:33 -0500

    Chnaged spawn points of resources

--- Assets/_Scripts/ResourceSpawn.cs
+++ Assets/_Scripts/ResourceSpawn.cs
@@ -39,51 +39,51 @@ public class ResourceSpawn : MonoBehaviour
 
         if(woodSpawnTimer > 6)
         {
-            spawnPointX = Random.Range(-75, 75);
-            spawnPointZ = Random.Range(-50, 50);
-            myPool.SpawnObject("Resource(Wood)", new Vector3(spawnPointX, 0.5f, spawnPointZ), transform.rotation, true);
+            spawnPointX = Random.Range(-46, -70);
+            spawnPointZ = Random.Range(-9, 15);
+            myPool.SpawnObject("Resource(Wood)", new Vector3(spawnPointX, 124.5f, spawnPointZ), transform.rotation, true);
             Debug.Log("Spawn Wood");
             woodSpawnTimer = 0;
         }
 
         if(stoneSpawnTimer > 8)
         {
-            spawnPointX = Random.Range(-75, 75);
-            spawnPointZ = Random.Range(-50, 50);
-            myPool.SpawnObject("Resource(Stone)", new Vector3(spawnPointX, 0.5f, spawnPointZ), transform.rotation, true);
+            spawnPointX = Random.Range(51, 73);
+            spawnPointZ = Random.Range(-8, 11);
+            myPool.SpawnObject("Resource(Stone)", new Vector3(spawnPointX, 124, spawnPointZ), transform.rotation, true);
             Debug.Log("Spawn Stone");
             stoneSpawnTimer = 0;
         }
 
         if(crystalSpawnTimer > 10)
         {
-            spawnPointX = Random.Range(-75, 75);
-            spawnPointZ = Random.Range(-50, 50);
-            myPool.SpawnObject("Resource(Crystal)", new Vector3(spawnPointX, 0.5f, spawnPointZ), transform.rotation, true);
+            spawnPointX = Random.Range(-9, 14);
+            spawnPointZ = Random.Range(-9, 14);
+            myPool.SpawnObject("Resource(Crystal)", new Vector3(spawnPointX, 115, spawnPointZ), transform.rotation, true);
             Debug.Log("Spawn Crystal");
             crystalSpawnTimer = 0;
         }
          if(woodSmallSpawnTimer > 2)
         {
-            spawnPointX = Random.Range(-75, 75);
-            spawnPointZ = Random.Range(-50, 50);
-            myPool.SpawnObject("Resource(WoodSmall)", new Vector3(spawnPointX, 0.5f, spawnPointZ), transform.rotation, true);
+            spawnPointX = Random.Range(-46, -70);
+            spawnPointZ = Random.Range(-9, 15);
+            myPool.SpawnObject("Resource(WoodSmall)", new Vector3(spawnPointX, 124.5f, spawnPointZ), transform.rotation, true);
             Debug.Log("Spawn Small Wood");
             woodSmallSpawnTimer = 0;
         }
          if(stoneSmallSpawnTimer > 3)
         {
-            spawnPointX = Random.Range(-75, 75);
-            spawnPointZ = Random.Range(-50, 50);
-            myPool.SpawnObject("Resource(StoneSmall)", new Vector3(spawnPointX, 0.5f, spawnPointZ), transform.rotation, true);
+            spawnPointX = Random.Range(51, 73);
+            spawnPointZ = Random.Range(-8, 11);
+            myPool.SpawnObject("Resource(StoneSmall)", new Vector3(spawnPointX, 124, spawnPointZ), transform.rotation, true);
             Debug.Log("Spawn Small Stone");
             stoneSmallSpawnTimer = 0;
         }
          if(crystalSmallSpawnTimer > 4)
         {
-            spawnPointX = Random.Range(-75, 75);
-            spawnPointZ = Random.Range(-50, 50);
-            myPool.SpawnObject("Resource(CrystalSmall)", new Vector3(spawnPointX, 0.5f, spawnPointZ), transform.rotation, true);
+            spawnPointX = Random.Range(-9, 14);
+            spawnPointZ = Random.Range(-9, 14);
+            myPool.SpawnObject("Resource(CrystalSmall)", new Vector3(spawnPointX, 115, spawnPointZ), transform.rotation, true);
             Debug.Log("Spawn Small Crystal");
             crystalSmallSpawnTimer = 0;
         }
