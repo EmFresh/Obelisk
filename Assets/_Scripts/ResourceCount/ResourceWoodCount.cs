11c684993b9736c46859466b6f30a340861bc394
Lillian <ziqi.fan@ontariotech.net>
2020-01-22 13:20:39 -0500

    Rouge Inventory Implemented

---
+++ Assets/_Scripts/ResourceCount/ResourceWoodCount.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceWoodCount : MonoBehaviour
+{
+    private Text woodText;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        woodText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        woodText.text = "" + player.GetComponent<PlayerPickup>().WoodAmount;
+    }
+}
