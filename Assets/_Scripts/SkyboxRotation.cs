d27993c4784d8e8598785aff4cf1e14183b9b4b0
Amtoj <amtojwork@gmail.com>
2020-02-14 00:15:25 -0500

    Fixed the lighting, added shadows back, added a script to make the skybox spin, and added Rowan's new build site

---
+++ Assets/_Scripts/SkyboxRotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxRotation : MonoBehaviour
+{
+    public float rotationSpeed = 1.33f;
+    // Update is called once per frame
+    void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * rotationSpeed);
+    }
+}
