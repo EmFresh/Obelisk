2fc211dc71b8b77d19010b115c9eebd5e0b52455
Rob <robert.andersen@ontariotechu.net>
2020-03-03 12:30:46 -0500

    Auto stash before merge of "Networking" and "origin/Networking"

---
+++ Assets/_Scripts/Networking/Chat.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+using Unity.Jobs;
+using static Networking;
+using static Networking.PResult;
+
+
+public class Chat : MonoBehaviour
+{
+
+    IPEndpointData serverIP;
+    // NetworkJob job;
+    SocketData sock;
+    //Socket socket = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+    private void Awake()
+    {
+        Networking.initNetwork();
+        serverIP = createIPEndpointData("localhost", 8888);
+        sock = initSocketData();
+
+        string test = "Strings are wack bro";
+        if (createSocket(ref sock, SocketType.UDP) != P_GenericError)
+        {
+            if (sendToPacket(ref sock, ref test, test.Length, ref serverIP) == P_GenericError)
+                print(getLastNetworkError());
+
+        }
+        print(getLastNetworkError());
+
+
+    }
+
+
+    private void OnApplicationQuit()
+    {
+        if (closeSocket(ref sock) == P_GenericError) print(getLastNetworkError());
+        shutdownNetwork();
+    }
+}
