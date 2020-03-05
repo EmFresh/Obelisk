b8d279f1fb93b8c5b439fc4947edcb2fb208d4bf
Rob <robert.andersen@ontariotechu.net>
2020-03-05 13:42:07 -0500

    Menu Scene Functional. LINKS TO THE LOBBY SCENE @Programmers

---
+++ Assets/_Scripts/Menu/ChangeScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    //public string sceneName;
+    [SerializeField] private string scene;
+    public void SceneChange(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+}
