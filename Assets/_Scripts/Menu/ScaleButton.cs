b8e3e3c0bb62463639aed31bcc4626ae1edd9ec0
Rob <robert.andersen@ontariotechu.net>
2020-03-12 13:30:04 -0400

    Menu button sounds added. Obelisk particle effects on menu added.

--- Assets/_Scripts/Menu/ScaleButton.cs
+++ Assets/_Scripts/Menu/ScaleButton.cs
@@ -4,30 +4,41 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using FMOD.Studio;
+using FMODUnity;
 
 public class ScaleButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+
+    [FMODUnity.EventRef] public string Event;
+    EventInstance instance;
+    EventDescription eventDescription;
 
     bool mouseOver = false;
     bool mouseOverExit = false;
     Vector3 minScale;
     Vector3 currentScale;
     public Vector3 targetScale;
-    public float  targetScaleAfter;
+    public float targetScaleAfter;
     private Vector3 maxScale;
 
-    [FMODUnity.EventRef]
-    public string hover = "";
-    FMOD.Studio.EventInstance hoverEvent;
+    //[FMODUnity.EventRef]
+    //public string hover = "";
+    //FMOD.Studio.EventInstance hoverEvent;
+    //[Range(0, 1)] public float maxVol = 1;
+    //float vol = 0;
     private void Start()
     {
         maxScale = targetScale * targetScaleAfter;
+       eventDescription = RuntimeManager.GetEventDescription(Event);
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
         mouseOver = true;
         mouseOverExit = false;
+        eventDescription.createInstance(out instance);
+        instance.start();
     }
 
     public void OnPointerExit(PointerEventData eventData)
@@ -40,9 +51,9 @@ public class ScaleButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
     {
         if (mouseOver)
         {
-            
+
             minScale = transform.localScale;
-            hoverEvent.start();
+            //hoverEvent.start();
             transform.localScale = Vector2.Lerp(minScale, maxScale, .25f);
         }
 
