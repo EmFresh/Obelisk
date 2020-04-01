c2fa748866c1a2947fcb1a6bc12ccfe83d896c8e
EmFresh <emory.wynn@uoit.net>
2020-03-31 20:13:33 -0400

    kill me now

--- Assets/_Scripts/Networking/NetworkControl.cs
+++ Assets/_Scripts/Networking/NetworkControl.cs
@@ -59,7 +59,7 @@ public class NetworkControl : MonoBehaviour
             type = MessageType.Movement; //int
             size = Marshal.SizeOf<Movement>(); //int
         }
-        public char[] data = new char[80];
+        public long l1,l2,l3,l4,l5,l6,l7,l8,l9,l10,l11,l12,l13,l14,l15;
     }
 
     [StructLayout(LayoutKind.Sequential)]
